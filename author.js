3f3e64690958902f332da743037a7fad5c9cd23f
grafisindo <36389972+grafisindo@users.noreply.github.com>
2018-02-12 09:27:22 +0800

    Create author.js

---
+++ author.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.grafisindo.com/") {
+        window.location.href = "http://www.grafisindo.com"; }});
+//]]>
